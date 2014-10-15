0c8a4b8d8f7d1da2abeb8a149838a6ca1e6a7220
Joshua "jag" Ginsberg <jag@flowtheory.net>
2014-10-15 13:43:05 -0400

    Fix affecting Django < 1.6

--- simplesync/models.py
+++ simplesync/models.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 
 import json
 
+import django
 from django.db import models
 from django.db.models import signals
 from django.core.serializers import serialize, deserialize
@@ -17,38 +18,44 @@ class ModelSyncer(object):
     def __init__(self, model):
         self.model = model
 
+    def get_model_name(self, model_cls):
+        if django.VERSION < (1,6):
+            return model_cls._meta.module_name
+        else:
+            return model_cls._meta.model_name
+
     def post_save_handler(self, sender=None, instance=None, created=None,
                           raw=None, using=None, update_fields=None, **kwargs):
         from . import tasks
         if raw:
             logger.warning('Received "raw" save request for %s %s - declining '
-                           'to operate', sender._meta.model_name, instance.pk)
+                           'to operate', self.get_model_name(sender), instance.pk)
             return
         if created:
             if not self.can_create(instance):
                 logger.debug('Received create signal for %s %s - but not '
                              'authorized by can_create',
-                             sender._meta.model_name, instance.pk)
+                             self.get_model_name(sender), instance.pk)
                 return
             result = tasks.do_sync.delay('create',
                                          sender._meta.app_label,
-                                         sender._meta.model_name,
+                                         self.get_model_name(sender),
                                          self.to_json(instance))
             logger.info('CREATE - %s %s - queued as %s',
-                        sender._meta.model_name, instance.pk, result.id)
+                        self.get_model_name(sender), instance.pk, result.id)
             return
         else:
             if not self.can_update(instance):
                 logger.debug('Received update signal for %s %s - but not '
                              'authorized by can_update',
-                             sender._meta.model_name, instance.pk)
+                             self.get_model_name(sender), instance.pk)
                 return
             result = tasks.do_sync.delay('update',
                                          sender._meta.app_label,
-                                         sender._meta.model_name,
+                                         self.get_model_name(sender),
                                          self.to_json(instance))
             logger.info('UPDATE - %s %s - queued as %s',
-                        sender._meta.model_name, instance.pk, result.id)
+                        self.get_model_name(sender), instance.pk, result.id)
             return
 
     def post_delete_handler(self, sender=None, instance=None, using=None,
@@ -57,7 +64,7 @@ class ModelSyncer(object):
         if not self.can_delete(instance):
             logger.debug('Received delete signal for %s %s - but not '
                          'authorized by can_delete',
-                         sender._meta.model_name, instance.pk)
+                         self.get_model_name(sender), instance.pk)
             return
         if hasattr(instance, 'natural_key') and \
                 hasattr(instance._default_manager, 'get_by_natural_key'):
@@ -67,10 +74,10 @@ class ModelSyncer(object):
             value = instance.pk
             json_body = {attname: value}
         result = tasks.do_sync.delay(
-            'delete', sender._meta.app_label, sender._meta.model_name,
+            'delete', sender._meta.app_label, self.get_model_name(sender),
             json.dumps(json_body, cls=DateTimeAwareJSONEncoder))
         logger.info('DELETE - %s %s - queued as %s',
-                    sender._meta.model_name, json_body, result.id)
+                    self.get_model_name(sender), json_body, result.id)
 
     def m2m_changed_handler(self, sender=None, instance=None, action=None,
                             reverse=None, model=None, pk_set=None,
@@ -83,17 +90,17 @@ class ModelSyncer(object):
             for pk in pk_set:
                 ThroughClass = sender
                 obj = ThroughClass.objects.get(
-                    **{type(instance)._meta.model_name: instance,
-                       model._meta.model_name: pk}
+                    **{self.get_model_name(type(instance)): instance,
+                       self.get_model_name(model): pk}
                 )
                 # We need a JSON generator with this model now...
                 syncer = type(self)(ThroughClass)
                 result = tasks.do_sync.delay('create',
                                              sender._meta.app_label,
-                                             sender._meta.model_name,
+                                             self.get_model_name(sender),
                                              syncer.to_json(obj))
                 logger.info('CREATE - %s %s - queued as %s',
-                            sender._meta.model_name, instance.pk, result.id)
+                            self.get_model_name(sender), instance.pk, result.id)
             return
         if action == 'post_remove' and \
                 self.can_remove_m2m(type(instance), model):
@@ -104,32 +111,32 @@ class ModelSyncer(object):
                     if hasattr(type(obj), 'natural_key') and \
                             hasattr(type(obj)._default_manager,
                                     'get_by_natural_key'):
-                        json_body[type(obj)._meta.model_name] = obj.natural_key()
+                        json_body[self.get_model_name(type(obj))] = obj.natural_key()
                     else:
                         attname = 'pk'
                         value = obj.pk
-                        json_body['%s__%s' % (type(obj)._meta.model_name,
+                        json_body['%s__%s' % self.get_model_name((type(obj)),
                                               attname)] = value
             result = tasks.do_sync.delay(
-                'delete', sender._meta.app_label, sender._meta.model_name,
+                'delete', sender._meta.app_label, self.get_model_name(sender),
                 json.dumps(json_body, cls=DateTimeAwareJSONEncoder))
             logger.info('DELETE - %s %s - queued as %s',
-                        sender._meta.model_name, json_body, result.id)
+                        self.get_model_name(sender), json_body, result.id)
         if action == 'post_clear' and \
                 self.can_remove_m2m(type(instance), model):
             if hasattr(type(instance), 'natural_key') and \
                     hasattr(type(instance)._default_manager,
                             'get_by_natural_key'):
-                json_body[type(instance)._meta.model_name] = instance.natural_key()
+                json_body[self.get_model_name(type(instance))] = instance.natural_key()
             else:
                 attname = 'pk'
                 value = instance.pk
                 json_body = {attname: value}
             result = tasks.do_sync.delay(
-                'delete', sender._meta.app_label, sender._meta.model_name,
+                'delete', sender._meta.app_label, self.get_model_name(sender),
                 json.dumps(json_body, cls=DateTimeAwareJSONEncoder))
             logger.info('DELETE - %s %s - queued as %s',
-                        sender._meta.model_name, json_body, result.id)
+                        self.get_model_name(sender), json_body, result.id)
 
     def can_add_m2m(self, model, other_model):
         return is_registered(model) and is_registered(other_model)
@@ -161,7 +168,7 @@ class SyncerRegistry(object):
     def register(self, model, cls):
         if model in self.registered:
             logger.info('Model %s is already registered for syncing',
-                        model._meta.model_name)
+                        self.get_model_name(model))
             return
         instance = cls(model)
         signals.post_save.connect(instance.post_save_handler, sender=model)
